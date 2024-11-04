de96bc2b91fcb57c61f42b181a005c3ef0eb4bf1
Deniz Bilgin <denizbilgin156@gmail.com>
2024-11-04 18:14:00 +0300

    bisiler

--- obj/Debug/net8.0/ObiGayrimenkul.AssemblyInfo.cs
+++ obj/Debug/net8.0/ObiGayrimenkul.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ObiGayrimenkul")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a6deb52351807c7fcebc1feb18f00bf52b30776")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ff802cbd2e33e6ad683e88b7c015edd49461c70")]
 [assembly: System.Reflection.AssemblyProductAttribute("ObiGayrimenkul")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ObiGayrimenkul")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
