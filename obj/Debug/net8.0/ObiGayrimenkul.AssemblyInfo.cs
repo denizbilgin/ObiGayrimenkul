bf411ed4a4eb78775a581d34e3445cccc207373c
Deniz Bilgin <denizbilgin156@gmail.com>
2024-11-20 19:31:44 +0300

    user profile bugu düzeltildi

--- obj/Debug/net8.0/ObiGayrimenkul.AssemblyInfo.cs
+++ obj/Debug/net8.0/ObiGayrimenkul.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ObiGayrimenkul")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c06c9d5620076969c83ab42a8c165ee20766f821")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+245e512cb19d6edd1f83ee3562fff3a004374af6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ObiGayrimenkul")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ObiGayrimenkul")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
